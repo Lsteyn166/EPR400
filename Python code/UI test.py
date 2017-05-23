56bfad3b49d7ac2de9aa7ae47b11840d4f912424
Lsteyn166 <lsteyn166@gmail.com>
2017-05-23 13:04:06 +0200

    UI code

--- Python code/UI test.py
+++ Python code/UI test.py
@@ -1,42 +1,66 @@
 import sys
-from PyQt4.QtGui import *
-from PyQt4.QtCore import *
+from PyQt4 import QtGui
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d.art3d as art3d
+
+globalcount = 0
+
+def updatecount():
+    global globalcount
+    globalcount= globalcount + 1
+
+class Window(QtGui.QDialog):
+    def __init__(self, parent=None):
+        count = 0
+        super(Window, self).__init__(parent)
+
+        self.count = 0
+
+        # a figure instance to plot on
+        self.figure = plt.figure()
+
+        # this is the Canvas Widget that displays the `figure`
+        # it takes the `figure` instance as a parameter to __init__
+        self.canvas = FigureCanvas(self.figure)
 
 
-class Example(QWidget):
-    def __init__(self):
-        super(Example, self).__init__()
-        self.initUI()
+        # Just some button connected to `plot` method
+        self.button = QtGui.QPushButton('Plot')
+        self.button.clicked.connect(self.plot)
 
-    def initUI(self):
-        self.text = "hello world"
-        self.setGeometry(100, 100, 400, 300)
-        self.setWindowTitle('Draw Demo')
-        self.show()
+        # set the layout
+        layout = QtGui.QVBoxLayout()
+        layout.addWidget(self.canvas)
+        layout.addWidget(self.button)
+        self.setLayout(layout)
+        #self.showMaximized()
 
-    def paintEvent(self, event):
-        qp = QPainter()
-        qp.begin(self)
-        qp.setPen(QColor(Qt.red))
-        qp.setFont(QFont('Arial', 20))
+    def plot(self):
 
-        qp.drawText(10, 50, "hello Python")
-        qp.setPen(QColor(Qt.blue))
-        qp.drawLine(10, 100, 100, 100)
-        qp.drawRect(10, 150, 150, 100)
+        updatecount()
 
-        qp.setPen(QColor(Qt.yellow))
-        qp.drawEllipse(100, 50, 100, 50)
-        qp.drawPixmap(220, 10, QPixmap("python.jpg"))
-        qp.fillRect(200, 175, 150, 100, QBrush(Qt.SolidPattern))
-        qp.end()
+        LegX = [0, globalcount]
+        LegY = [0, globalcount]
+        LegZ = [0, globalcount]
+
+        # create an axis
+        ax = self.figure.add_subplot(111, projection = '3d')
 
 
-def main():
-    app = QApplication(sys.argv)
-    ex = Example()
-    sys.exit(app.exec_())
+        # discards the old graph
+        ax.hold(False)
 
+        # plot data
+        ax.plot(LegX, LegY, LegZ, color='#00FF00')
+        ax.set_xlabel('X')
+
+        # refresh canvas
+        self.canvas.draw()
 
 if __name__ == '__main__':
-    main()
+    app = QtGui.QApplication(sys.argv)
+    main = Window()
+    main.show()
+
+    sys.exit(app.exec_())
